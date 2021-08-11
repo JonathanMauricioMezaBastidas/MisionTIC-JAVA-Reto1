6705ec76bd4033a32c3694758f8bf7633ca15d08
Jonathan Mauricio Meza Bastidas <mbjonathan@misena.edu.co>
2021-08-10 21:04:34 -0500

    Reto 1  - JAVA

---
+++ src/reto1/Reto1.java
@@ -0,0 +1,40 @@
+package reto1;
+
+import java.util.Scanner;
+
+public class Reto1 {
+
+    public static void rascacielos(int value) {
+        int ladrillo =  value;
+        int cemento = ladrillo * 2;
+        cemento = cemento + 4;
+        int bloque = ladrillo + cemento;
+        bloque = bloque / 5;
+        System.out.println(ladrillo+" "+cemento+" "+bloque);
+        
+        String fase = "";
+        if (bloque >= 0 && bloque <= 20) {
+            //System.out.println("uno");
+            fase = "uno";
+        } else if (bloque >= 21 && bloque <= 30) {
+            //System.out.println("dos");
+            fase = "dos";
+        } else if (bloque >= 31 && bloque <= 50) {
+            //System.out.println("tres");
+            fase = "tres";
+        } else {
+            //System.out.println("cuatro");
+            fase = "cuatro";
+        }
+        System.out.println(fase);
+        
+    }
+
+    public static void main(String[] args) {
+        Scanner consola = new Scanner(System.in);
+        int pisos = consola.nextInt();
+        rascacielos(pisos);
+        
+    }
+
+}
